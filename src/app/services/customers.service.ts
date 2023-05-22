cd3151b609ea688d218ae47969eb37cbea54d378
laura hoyos <laura.hoyos.suarez@gmail.com>
2023-05-22 17:58:31 -0500

    create addCustomer with a return, install rxjs and import de same module

--- src/app/services/customers.service.ts
+++ src/app/services/customers.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  addCustomer(data: any): Observable<any> {
+    return this._http.post('http://localhost:3000/customer', data);
+  }
 }
