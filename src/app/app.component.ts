d844007d68d84df7595b9d11cafcd2805d5b0894
laura hoyos <laura.hoyos.suarez@gmail.com>
2023-05-22 19:28:41 -0500

    adding ngOnInit()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { CustomersService } from './services/customers.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud-app';
 
-  constructor(private _dialog: MatDialog){}
-    openAddEditEmpForm(){
+  constructor(private _dialog: MatDialog, private _empService: CustomersService){}
+  
+  ngOnInit(): void {
+    this.getCustomerList();
+  }
+  
+  openAddEditEmpForm(){
       this._dialog.open(EmpAddEditComponent)
     }
       
-    
+    getCustomerList() {
+      this._empService.getCustomerList().subscribe({
+        next: (res) => {
+
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
+    }
   
 }
